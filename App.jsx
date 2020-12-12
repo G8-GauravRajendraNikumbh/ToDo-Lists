6f597700d3892d38801abfaadb0c9986798c6469
gauravnikumbh88@gmail.com <58488006+G8-GauravRajendraNikumbh@users.noreply.github.com>
2020-12-12 19:50:16 +0530

    Add files via upload
    Create ToDo Lists Application using Reactjs.

---
+++ App.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useState } from 'react';
+import  ToDoLists from'./ToDoLists';
+
+const App = () => {
+
+    const [inputList, setInputList] = useState("");
+    const [items, setItems] = useState([]);
+
+    const itemEvent = (event) =>{
+        setInputList(event.target.value);
+        
+    };
+
+    const listOfItems = ()=>{
+        setItems((oldItems)=>{
+            return [...oldItems, inputList];
+        });
+        setInputList('');
+    };
+    const deleteItems = (id) =>{
+        console.log('deleted');
+        setItems((oldItems)=>{
+            return oldItems.filter((arrElem,index)=>{
+                return index !== id; 
+
+            })
+        })
+    };
+    return <>
+        <div className = "main_div">
+            <div className = "center_div">
+                <br/>
+                <h1>ToDo List</h1>
+                
+                <br/>
+                <input type = "text"  placeholder = "Add item" onChange = {itemEvent} value = {inputList}/>
+                <button onClick = {listOfItems}>+</button>            
+                <ol>
+                    {/* <li>{inputList}</li> */}
+                    {items.map( (itetmval, index)=>{
+                      return <ToDoLists key = {index} id = {index} text= {itetmval} onSelect = {deleteItems}/>
+                    })}
+                </ol>
+            </div>
+        
+            </div>   
+         </>
+
+}
+export default App;
